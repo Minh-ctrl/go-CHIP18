01bd1db97801c6d42d96643811e3992c4b2e790b
minh-ctrl <binhminh140899@gmail.com>
2023-10-20 03:17:25 +0900

    draw pixels function@

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"image/color"
 	_ "image/png"
 	"log"
+	"math"
 
 	monitor "github.com/Minh-ctrl/go-CHIP18.git/monitor"
+	chip8 "github.com/Minh-ctrl/go-CHIP18.git/struct"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
@@ -31,6 +33,41 @@ func (g *Game) Update() error {
 	return nil
 }
 
+var frame chip8.Chip8
+
+func setPixel(x int, y int) {
+	//
+	if x > monitor.Columns {
+		x -= monitor.Columns
+	} else if x < 0 {
+		x += monitor.Columns
+	}
+	if y > monitor.Rows {
+		y -= monitor.Rows
+	} else if y < 0 {
+		y += monitor.Rows
+	}
+	var displayIndex = x + (y * monitor.Columns)
+	// flip the value
+	frame.Framebuffer[displayIndex] = !frame.Framebuffer[displayIndex]
+	// cant return? need to go through golang again
+}
+
+//	func testRender() {
+//		setPixel(12, 32)
+//		setPixel(2, 1)
+//	}
+func paint(screen *ebiten.Image) {
+	for i := 0; i < monitor.Columns*monitor.Rows; i++ {
+		var x = (i % monitor.Columns) * monitor.Scale
+		var y = math.Floor(float64(i)/monitor.Columns) * monitor.Scale
+
+		if frame.Framebuffer[i] {
+			vector.DrawFilledRect(screen, float32(x), float32(y), rectangleW, rectangleH, color.White, false)
+		}
+	}
+}
+
 func (g *Game) Draw(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(float64(frameWidth)*monitor.Scale, float64(frameHeight)*monitor.Scale)
@@ -38,22 +75,19 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	// define the x and y
 
 	screen.Fill(color.Black)
-	// coordinates
-	// populate
-	for i := 0; i < monitor.Columns; i++ {
+	// draw is being run over and over again
+	setPixel(64, 2)
+	setPixel(64, 3)
 
-		for j := 0; j < monitor.Rows; j++ {
-			if (i+j)%2 == 0 {
-				vector.DrawFilledRect(screen, float32(i)*monitor.Scale, float32(j)*monitor.Scale, rectangleW, rectangleH, color.White, false)
-
-			}
-
-			// if frame.Framebuffer[i] {
-			// 	log.Println("placeholder")
-			// }
-		}
-	}
+	paint(screen)
 }
+
+// func clear(frameBuffer[]bool) {
+// 	// nah this doesnt work
+// 	var newFrameBuffer = frame.Framebuffer
+// 	// reset state
+// 	// frameBuffer = newFrameBuffer
+// }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
--- monitor/Monitor.go
+++ monitor/Monitor.go
@@ -1,37 +1,7 @@
 package monitor
 
-import (
-	chip8 "github.com/Minh-ctrl/go-CHIP18.git/struct"
-)
-
 const (
 	Columns = 64
 	Rows    = 32
 	Scale   = 15
 )
-
-var frame chip8.Chip8
-
-func setPixel(x int, y int, frameBuffer [64 * 32]bool) {
-	//
-	if x > Columns {
-		x -= Columns
-	} else if x < 0 {
-		x += Columns
-	}
-	if y > Rows {
-		y -= Rows
-	} else if y < 0 {
-		y += Rows
-	}
-	var displayIndex = x + (y * Columns)
-	// flip the value
-	frameBuffer[displayIndex] = !frameBuffer[displayIndex]
-	// cant return? need to go through golang again
-}
-
-func clear(frameBuffer [64 * 32]bool) {
-	var newFrameBuffer = frame.Framebuffer
-	// reset state
-	frameBuffer = newFrameBuffer
-}
